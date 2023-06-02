baf62feb34614fb74e49bb7f636c527b223e5847
juanuranidev <juanuranidev@gmail.com>
2023-06-01 23:37:40 -0300

    Created function to connect to mongodb

---
+++ config/mongodbApiConnext.ts
@@ -0,0 +1,18 @@
+import { connect, connection } from "mongoose";
+
+const mongodbConnect = async () => {
+  const conn = {
+    isConnected: false,
+  };
+
+  if (conn.isConnected) return;
+
+  const db: any = await connect(process.env.NEXT_PUBLIC_MONGODB_URI!);
+  conn.isConnected = db.connections[0].readyState;
+};
+
+connection.on("connected", () => {
+  console.log("CONECTADO");
+});
+
+export default mongodbConnect;
