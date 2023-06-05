693cb809ebe0ec8057b3a380ecace7d599e8dbdf
juanuranidev <juanuranidev@gmail.com>
2023-06-04 23:52:35 -0300

    Finished calendar view index

--- pages/student/calendar/index.tsx
+++ pages/student/calendar/index.tsx
@@ -1,12 +1,6 @@
-import BottomNavbar from "layout/navbar/bottomNavbar/BottomNavbar";
 import React from "react";
 import Calendar from "views/student/calendar/Calendar";
 
 export default function index() {
-  return (
-    <>
-      <Calendar />
-      <BottomNavbar />;
-    </>
-  );
+  return <Calendar />;
 }
