459fc40bdf9fd5eb23122c1e2b2e56dc42273bcf
juanuranidev <juanuranidev@gmail.com>
2023-06-18 23:57:40 -0300

    Updated calendar view

--- pages/student/calendar/index.tsx
+++ pages/student/calendar/index.tsx
@@ -1,6 +1,25 @@
+import { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
 import React from "react";
 import Calendar from "views/student/calendar/Calendar";
 
 export default function index() {
   return <Calendar />;
 }
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
